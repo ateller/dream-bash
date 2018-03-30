8f359d889ba8e68b754147a5d6eeb2359763f362
ateller <tony.tel@yandex.ru>
2018-03-30 16:53:21 +0300

    signals and mad error handling

--- dream-bash.c
+++ dream-bash.c
@@ -10,24 +10,32 @@
 #include <ctype.h>
 #include <fcntl.h>
 #include <sys/wait.h>
+#include <signal.h>
 
 #define INP_SIZE 256
 #ifndef IF_ERR
-#define IF_ERR(check, error, info)\
+#define IF_ERR(check, error, info, handle)\
 	do { \
 		if (check == error) { \
 			perror(info);\
-			exit(errno);\
+			handle\
 		} \
 	} while (0)
 #endif
-
 #define ENV_SIZE 512
 #define INVITE "%s@%s:%s$ ", user, hostname, cwd
 //Я не настолько хорошо знаю английский,
 //чтобы писать на нем комментарии и не
 //позориться, а исправлять перевод
 //после гугла мне сейчас лень.
+#define DEF_HANDL 1
+#define SET_HANDL 0
+//Параметры функции signals_handler
+//0 - установить обрабочик, который
+//будет пересылать сигналы дочернему
+//процессу. 1 - вернуть дефолт
+
+int child_pid;
 
 void tilda(char *path, char *user)
 //Заменяет в пути домашний каталог на ~
@@ -74,16 +82,25 @@ char **split(char *input)
 	int i = 0;
 
 	args = malloc(sizeof(char *));
-	IF_ERR(args, NULL, "Malloc error:");
+	IF_ERR(args, NULL, "Malloc error:", return NULL;);
 	//Выделяем память под массив
 	//аргументов
 	args[i] = strtok(input, " \t\n");
 	//Бьем строку
 	for (; args[i] != NULL; ) {
+		char **dup_args;
+		//Сохраним сюда указатель перед
+		//реаллоком
+
 		i++;
+		dup_args = args;
 		args = realloc(args, (i + 1) * sizeof(char *));
-		IF_ERR(args, NULL, "Realloc error:");
 		//Увеличиваем вектор
+		if (args == NULL) {
+			perror("Realloc error");
+			free(dup_args);
+			return NULL;
+		}
 		args[i] = strtok(NULL, " \t\n");
 		//Записываем очередную лексему
 	}
@@ -91,35 +108,65 @@ char **split(char *input)
 	return args;
 }
 
+void send_sig(int sig)
+{
+	if (child_pid)
+		IF_ERR(kill(child_pid, sig), -1, "Kill error", ;);
+	//Пересылаем сигнал потомку
+}
+
+void signals_handler(int mode)
+{
+	struct sigaction act;
+
+	if (mode)
+		act.sa_handler = SIG_DFL;
+	else
+		act.sa_handler = send_sig;
+	//Вернуть деф. обработчик
+	//или поставить новый
+	sigemptyset(&act.sa_mask);
+	//Пока не вижу необходимости
+	//что-то игнорировать
+	act.sa_flags = SA_RESTART;
+	//Я не знаю точно, но мне
+	//кажется, так будет лучше
+	IF_ERR(sigaction(SIGINT, &act, NULL), -1, "Sigacion error", ;);
+	IF_ERR(sigaction(SIGTSTP, &act, NULL), -1, "Sigacion error", ;);
+	IF_ERR(sigaction(SIGCONT, &act, NULL), -1, "Sigacion error", ;);
+	//Пересылаем сигналы прерывания,
+	//приостановки и возобновления
+}
+
 void run(char **args)
 {
 	int i = 0;
-	int pid;
 
 	for (; args[i] != NULL; i++)
 		printf("Arg %d is %s\n", i, args[i]);
-	pid = fork();
+	child_pid = fork();
 	//Копируем процесс
-	switch (pid) {
+	switch (child_pid) {
 	case -1:
 		//если ошибка
 		perror("Fork error:");
-		exit(errno);
-		break;
+		return;
 	case 0:
 		i = execvp(args[0], args);
 		//Запускаем программу
 		if (errno != ENOENT)
-			IF_ERR(i, -1, "Exec error");
-		else 
+			IF_ERR(i, -1, "Exec error", ;);
+		else
 			perror(args[0]);
 		//Если такого файла нету,
 		//перрор печатает
 		break;
 	default:
-		wait(&i);
-		//Возможно потом обработаю i
+		signals_handler(SET_HANDL);
+		IF_ERR(wait(&i), -1, "Wait error", kill(child_pid, 9););
 		//Ждем процесс
+		signals_handler(DEF_HANDL);
+		printf("\n");
 	}
 }
 void recognize(char **args)
@@ -129,7 +176,7 @@ void recognize(char **args)
 {
 	if (!strcmp(args[0], "cd")) {
 	//Если во вводе cd
-		if(chdir(args[1]) == -1)
+		if (chdir(args[1]) == -1)
 			perror(args[1]);
 		return;
 		//И возвращаемся
@@ -143,28 +190,36 @@ int main(int argc, char *argv[])
 	char buf[INP_SIZE], hostname[ENV_SIZE], *user, cwd[ENV_SIZE];
 	struct passwd *userinfo;
 
-	IF_ERR(gethostname(hostname, ENV_SIZE), -1, "Can't get hostname");
+	child_pid = 0;
+	//Обнуляем пид дочернего
+	if (-1 == gethostname(hostname, ENV_SIZE)) {
+		perror("Gethostname error");
+		strcpy(hostname, "UNKNOWN_MASHINE");
+	}
 	//узнаем имя машины
 	userinfo = getpwuid(getuid());
-	IF_ERR(userinfo, NULL, "Getpwiud error");
-	user = userinfo->pw_name;
+	if (userinfo == NULL) {
+		perror("Getpwiud error");
+		user = "UNKNOWN";
+	} else
+		user = userinfo->pw_name;
 	//и имя пользователя
 	history = open("history", O_RDWR|O_APPEND|O_CREAT, 0664);
-	IF_ERR(history, -1, "Open error");
+	IF_ERR(history, -1, "Open error", history = -1;);
 
 	while (true) {
 		int len;
 		char **args;
 
-		IF_ERR(getcwd(cwd, ENV_SIZE), NULL, "Getcwd error");
+		IF_ERR(getcwd(cwd, ENV_SIZE), NULL, "Cwd error", exit(errno););
 		//Узнаем текущий каталог
 		tilda(cwd, user);
-		IF_ERR(printf(INVITE), -1, "Printf error:");
+		IF_ERR(printf(INVITE), -1, "Printf error:", exit(errno););
 		//приглашаем
-		IF_ERR(fflush(stdout), EOF, "Fflush error:");
+		IF_ERR(fflush(stdout), EOF, "Fflush error:", ;);
 		//чтобы вывелось сразу
 		len = read(0, buf, INP_SIZE - 1);
-		IF_ERR(len, -1, "Can't read your input (read error):");
+		IF_ERR(len, -1, "Read error:", exit(errno););
 		//читаем ввод
 		buf[len] = 0;
 		//заканчиваем строку
